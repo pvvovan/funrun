d5fb4bee9b9fd3f075bcf508a9b9a9b3c2e28f74
PVVovan <pvvovan@gmail.com>
2024-12-01 08:41:46 +0100

    Build by cmake/ninja

---
+++ README.md
@@ -0,0 +1,8 @@
+Create project:
+cmake -S ./ --preset Debug
+
+Build project:
+cmake --build --preset Debug --parallel
+
+Clean project:
+cmake --build --preset Debug --target 'clean'
--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -8,7 +8,8 @@ set(CMAKE_CXX_COMPILER_ID GNU)
 
 # Some default GCC settings
 # arm-none-eabi- must be part of path environment
-set(TOOLCHAIN_PREFIX                arm-none-eabi-)
+set(GCC_PATH              "/home/vovan/mySTM/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/bin/")
+set(TOOLCHAIN_PREFIX                ${GCC_PATH}arm-none-eabi-)
 
 set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
